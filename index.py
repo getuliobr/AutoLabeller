1f54facc771eb81479288284e2680cd97c4aa64e
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-01-14 16:19:15 -0300

    trocar .env para config.ini e adicionar biblioteca do banco de dados

--- index.py
+++ index.py
@@ -2,10 +2,10 @@ from octokit import Octokit
 
 from dotenv import dotenv_values
 from flask import Flask, request, abort
-import hmac
+import hmac, json
+from config import config
 
 app = Flask(__name__)
-config = dotenv_values(".env")
 
 @app.before_request
 def verify_webhook_signature():
@@ -13,8 +13,8 @@ def verify_webhook_signature():
   request.headers['X_HUB_SIGNATURE']
   their_signature_header = request.headers['X_HUB_SIGNATURE']
   method, their_digest = their_signature_header.split('=')
-
-  key = bytes(config['GITHUB_WEBHOOK_SECRET'], 'utf8')
+  print(config['GITHUB']['WEBHOOK_SECRET'])
+  key = bytes(config['GITHUB']['WEBHOOK_SECRET'], 'utf8')
 
   our_digest = hmac.digest(key=key, msg=payload_raw, digest=method).hex()
 
@@ -23,17 +23,18 @@ def verify_webhook_signature():
 
 @app.route('/event_handler', methods=['POST'])
 def event_handler():
-  octokit = Octokit(auth='installation', app_id=config['GITHUB_APP_IDENTIFIER'], private_key=config['GITHUB_PRIVATE_KEY'])
+  octokit = Octokit(auth='installation', app_id=config['GITHUB']['APP_IDENTIFIER'], private_key=config['GITHUB']['PRIVATE_KEY'])
   payload = request.json
 
   if payload['action'] != 'opened':
     return abort(400)
 
   repo = payload['repository']['name']
-  issue_number = payload['issue']['number']
   owner = payload['repository']['owner']['login']
+  issue_number = payload['issue']['number']
+  body = payload['issue']['body']
 
-  print(owner, issue_number, repo)
+  print(owner, issue_number, repo, body)
 
   octokit.issues.add_labels_to_an_issue(owner=owner, repo=repo, issue_number=issue_number, labels=["needs-response"])
   octokit.issues.create_comment(owner=owner, repo=repo, issue_number=issue_number, body="teste direto do python")
